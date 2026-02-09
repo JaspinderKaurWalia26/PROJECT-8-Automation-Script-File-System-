1200a1d7d728a430c10c8e8487377ab88f977feb
Jaspinder Kaur <jaspinderkaurwalia855@gmail.com>
2026-02-09 11:49:42 +0530

    Added configuration file

---
+++ config.py
@@ -0,0 +1,8 @@
+# All folder paths and valid extensions
+
+INPUT_FOLDER = "input_folder"
+PROCESSED_FOLDER = "processed_files"
+ERROR_FOLDER = "error_files"
+LOG_FILE = "logs/automation.log"
+
+VALID_EXTENSIONS = [".txt", ".csv"]
